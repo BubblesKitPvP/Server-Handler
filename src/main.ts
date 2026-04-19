048d4d1b93d5234cc3a0a62860d595bd0f227f41
Espryra <espryra@gmail.com>
2026-04-18 19:20:22 -0600

    Added exit handler

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import Discord from "./modules/discord/discord";
 import Handler from "./modules/handler";
 import Cache from "./utils/cache";
+import ExitHandler from "./utils/exit/exit";
 
 await Cache.Init();
 await Handler.Init();
 await Discord.Init();
+
+new ExitHandler();
---
+++ src/utils/exit/exit.ts
@@ -0,0 +1,28 @@
+import Handler from "../../modules/handler";
+
+export default class ExitHandler {
+  public constructor() {
+    this.Init();
+  }
+
+  private Init(): void {
+    process.once("SIGTERM", async () => {
+      console.log("Shutting down...");
+
+      if (Handler.IsRunning()) {
+        Handler.Stop();
+
+        await new Promise((resolve) => {
+          const loop = setInterval(() => {
+            if (!Handler.IsRunning()) {
+              clearInterval(loop);
+              resolve(true);
+            }
+          });
+        });
+
+        process.exit(0);
+      }
+    });
+  }
+}
